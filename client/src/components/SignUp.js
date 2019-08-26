37418588b7745e33afb9cbdc4f18a4df4d0b86af
unknown <ahmadhamzah500@gmail.com>
2019-08-26 17:10:05 -0500

    Made style changes

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,61 +1,80 @@
-import React, {useState, useEffect} from 'react';
-import {Link} from 'react-router-dom';
-import fire from '../config/Fire';
-import {Container, Form, FormGroup, Button, Label, Input} from 'reactstrap';
-const SignUp = (props) => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [signUpError, setError] = useState('');
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import fire from "../config/Fire";
+import { Container, Form, FormGroup, Button, Label, Input } from "reactstrap";
+const SignUp = props => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [signUpError, setError] = useState("");
 
-    useEffect(() => {
-        fire.auth().signOut();
-    }, [])
-    // const onSubmit = (e) => {
-    //     e.preventDefault();
-    //     console.log(`The email is ${email} and the password is ${password}`);
-    // }
+  useEffect(() => {
+    fire.auth().signOut();
+  }, []);
+  // const onSubmit = (e) => {
+  //     e.preventDefault();
+  //     console.log(`The email is ${email} and the password is ${password}`);
+  // }
 
-    const signup =(e) => {
-        e.preventDefault();
-        fire.auth().createUserWithEmailAndPassword(email, password).then((u)=>{
-            props.history.push('/');
-        }).catch((error) => {
-            if(error.code === 'auth/invalid-email'){
-                setError('Please enter a valid email')
-            }
-            else if(error.code === 'auth/weak-password'){
-                setError('The password must be 6 characters long or more.')
-            }
-            else if (error.code === 'auth/email-already-in-use'){
-                setError('This email is already in use')
-            }
-            else{
-                console.log(error);
-                setError('Please enter a valid username and password')
-            }
-          });
+  const signup = e => {
+    e.preventDefault();
+    fire
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(u => {
+        props.history.push("/");
+      })
+      .catch(error => {
+        if (error.code === "auth/invalid-email") {
+          setError("Please enter a valid email");
+        } else if (error.code === "auth/weak-password") {
+          setError("The password must be 6 characters long or more.");
+        } else if (error.code === "auth/email-already-in-use") {
+          setError("This email is already in use");
+        } else {
+          console.log(error);
+          setError("Please enter a valid username and password");
+        }
+      });
+  };
+  return (
+    <div>
+      <Container>
+        <Form onSubmit={signup} className="auth-form">
+          <h1 style={{ textAlign: "center" }}>MERN To-Do List</h1>
+          <br />
+          <div className="auth-header">Sign Up:</div>
+          <hr />
+          <p className="alert-text">{signUpError}</p>
+          <FormGroup>
+            <Label for="email">Email:</Label>
+            <Input
+              type="text"
+              name="email"
+              id="email"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="password">Password:</Label>
+            <Input
+              type="password"
+              name="password"
+              id="password"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+            />
+          </FormGroup>
+          <Button className="form-btn" color="primary">
+            Submit
+          </Button>
+          <div className="form-bottom-text">
+            Already a member? <Link to="/">Sign In</Link>
+          </div>
+        </Form>
+      </Container>
+    </div>
+  );
+};
 
-    }
-    return (
-        <div>
-            <Container>
-                <Form onSubmit = {signup} className = 'auth-form'>
-                    <div className = 'auth-header'>Sign Up:</div>
-                    <p className = 'alert-text'>{signUpError}</p>
-                    <FormGroup>
-                    <Label for = 'email'>Email:</Label>
-                    <Input type = 'text' name = 'email' id = 'email' value = {email} onChange = {(e) => setEmail(e.target.value)}/>
-                    </FormGroup>
-                    <FormGroup>
-                    <Label for = 'password'>Password:</Label>
-                    <Input type = 'password' name = 'password' id = 'password' value = {password} onChange = {(e) => setPassword(e.target.value)}/>
-                    </FormGroup>
-                    <Button className = 'form-btn' color = 'primary'>Submit</Button>
-                    <div className = 'form-bottom-text'>Already a member? <Link to='/'>Sign In</Link></div>
-                </Form>
-            </Container>
-        </div>
-    )
-}
-
-export default SignUp
+export default SignUp;
--- client/src/components/Tasklist.js
+++ client/src/components/Tasklist.js
@@ -1,105 +1,135 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
-import fire from '../config/Fire';
-import { Button, Container, ListGroup, ListGroupItem, Form, FormGroup, Input, InputGroup, InputGroupText, InputGroupAddon, Navbar, Nav, NavItem, NavLink } from 'reactstrap';
-import {Helmet} from 'react-helmet';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import fire from "../config/Fire";
+import {
+  Button,
+  Container,
+  ListGroup,
+  ListGroupItem,
+  Form,
+  FormGroup,
+  Input,
+  InputGroup,
+  InputGroupText,
+  InputGroupAddon,
+  Navbar,
+  Nav,
+  NavItem,
+  NavLink
+} from "reactstrap";
 
-const Tasklist = (props) => {
+const Tasklist = props => {
+  const [items, setItems] = useState([]);
 
-    const [items, setItems] = useState(
-        []  
-      );
-    
-    const [newTask, setNewTask] = useState();
+  const [newTask, setNewTask] = useState();
 
-    const  onChange = (e) => {
-        setNewTask(e.target.value);
-    }
-    
-    const onSubmit = (e) => {
-        e.preventDefault();
-        axios({
-            method: 'post',
-            url: '/api/items',
-            data: {
-              id: props.userId,  
-              name: newTask
-            }
-          })
-          .then(res => setItems([...items, res.data]));
-          setNewTask('');
-          document.querySelector('#task-input-box').value = '';
-       
-    }
-    
-    const logout = () => {
-        fire.auth().signOut();
-    }
+  const onChange = e => {
+    setNewTask(e.target.value);
+  };
 
+  const onSubmit = e => {
+    e.preventDefault();
+    axios({
+      method: "post",
+      url: "/api/items",
+      data: {
+        id: props.userId,
+        name: newTask
+      }
+    }).then(res => setItems([...items, res.data]));
+    setNewTask("");
+    document.querySelector("#task-input-box").value = "";
+  };
 
-    useEffect(() => {
-        axios.get('/api/items').then(res => {
-            const tasks = res.data.filter(item => item.id === props.userId);
-            setItems(tasks);
-        });
-    }, []);
-        
-    return (
-        <div>
-            <Helmet>
+  const logout = () => {
+    fire.auth().signOut();
+  };
+
+  useEffect(() => {
+    axios.get("/api/items").then(res => {
+      const tasks = res.data.filter(item => item.id === props.userId);
+      setItems(tasks);
+    });
+  }, []);
+
+  return (
+    <Container>
+      {/* <Helmet>
                 <style>{'body {background: -webkit-linear-gradient(to right, #2980b9, #6dd5fa, #ffffff) !important; background: linear-gradient(to right, #2980b9, #6dd5fa, #ffffff) !important; }'}</style>
-            </Helmet>
-            <Navbar className = 'navbar navbar-transparent'>
-                <Container>
-                    <Nav className="ml-auto" navbar>
-                        <NavItem>  
-                            <Form onSubmit = {logout}>
-                                <div id = 'user-email'>{props.userEmail}</div>
-                                <Button color="info">Sign Out</Button>
-                            </Form>
-                        </NavItem>  
-                    </Nav>
-                </Container>
-            </Navbar>
-            <div id = 'text-input-field'>
-                <Form onSubmit = {onSubmit}>
-                    <InputGroup>
-                        <Input onChange = {onChange}  bsSize="lg" placeholder = 'Add task' id = 'task-input-box' required/>
-                        <InputGroupAddon addonType="append">
-                        {/* <Input type = 'submit' class="btn btn-lg" value = 'Add'/> */}
-                        <Button color="primary">Add</Button>
-                        </InputGroupAddon>
-                    </InputGroup>
-    
-                </Form>
-                <ListGroup>
-                    {items.map(item => {
-                        return(
-                                <ListGroupItem className = "list-group-item d-flex justify-content-between align-items-center">
-                                <InputGroupAddon addonType="prepend">
-                                    <Input addon type="checkbox" aria-label="Checkbox for following text input" style ={{margin: '50px !important'}}/>
-                                    
-                                </InputGroupAddon>
-                                
-                                {item.name}
-                                 
+            </Helmet> */}
 
-                                <Button onClick = {(e) => {
-                                    axios.delete(`/api/items/${item._id}`).then(
-                                        setItems(items.filter(currItem => currItem._id !== item._id))
-                                    );
+      <div id="text-input-field">
+        <Navbar
+          className="navbar navbar-transparent"
+          style={{ paddingRight: "0px", marginmRight: "0px" }}
+        >
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <Form onSubmit={logout}>
+                <div id="user-email">
+                  Signed in as: <br />
+                  {props.userEmail}
+                </div>
+                <br />
+                <Button color="info" className="float-right">
+                  Sign Out
+                </Button>
+              </Form>
+            </NavItem>
+          </Nav>
+        </Navbar>
+        <br />
+        <Form onSubmit={onSubmit}>
+          <InputGroup>
+            <Input
+              onChange={onChange}
+              bsSize="lg"
+              placeholder="Add task"
+              id="task-input-box"
+              required
+            />
+            <InputGroupAddon addonType="append">
+              {/* <Input type = 'submit' class="btn btn-lg" value = 'Add'/> */}
+              <Button color="primary">Add</Button>
+            </InputGroupAddon>
+          </InputGroup>
+        </Form>
+        <ListGroup>
+          {items.map(item => {
+            return (
+              <ListGroupItem className="list-group-item d-flex justify-content-between">
+                <InputGroupAddon addonType="prepend">
+                  <Input
+                    addon
+                    type="checkbox"
+                    aria-label="Checkbox for following text input"
+                    style={{ margin: "50px !important" }}
+                  />
+                </InputGroupAddon>
 
-                                }} color="danger">X</Button>
-                            </ListGroupItem>
-                            
+                {item.name}
 
+                <Button
+                  onClick={e => {
+                    axios
+                      .delete(`/api/items/${item._id}`)
+                      .then(
+                        setItems(
+                          items.filter(currItem => currItem._id !== item._id)
                         )
-                    })}
-                </ListGroup>
-            </div>
-        </div>
-    )
-}
+                      );
+                  }}
+                  color="danger"
+                >
+                  X
+                </Button>
+              </ListGroupItem>
+            );
+          })}
+        </ListGroup>
+      </div>
+    </Container>
+  );
+};
 
-export default Tasklist
-
+export default Tasklist;
